f5e4bdd4622f75f5694cb1b6d05fe3df28e34f7b
Aadil Khatri <aadil.khatri343@gmail.com>
2023-01-20 20:28:14 +0530

    Finish project

--- src/component/forecast/forecast.js
+++ src/component/forecast/forecast.js
@@ -5,6 +5,7 @@ import {
   AccordionItemHeading,
   AccordionItemPanel,
 } from "react-accessible-accordion";
+import "./forecast.css";
 
 const WEEK_DAYS = [
   "Monday",
@@ -47,7 +48,34 @@ const Forecast = ({ data }) => {
                   </div>
                 </AccordionItemButton>
               </AccordionItemHeading>
-              <AccordionItemPanel></AccordionItemPanel>
+              <AccordionItemPanel>
+                <div className="daily-details-grid">
+                  <div className="daily-details-grid-item">
+                    <label>Pressure:</label>
+                    <label>{item.main.pressure} hPa</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Humidity:</label>
+                    <label>{item.main.humidity}%</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Clouds:</label>
+                    <label>{item.clouds.all}%</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Wind Speed:</label>
+                    <label>{item.wind.speed} m/s</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Sea level:</label>
+                    <label>{item.main.sea_level}m</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Feels like:</label>
+                    <label>{Math.round(item.main.feels_like)}°C</label>
+                  </div>
+                </div>
+              </AccordionItemPanel>
             </AccordionItem>
           );
         })}
